bf46a380ab5e6484d5a6ba54b64f61d1d6ecbfe7
Pksubudhi25 <pratikkumarsubudhi@gmail.com>
2024-06-23 09:32:16 +0530

    Basics of component

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-
+import Chai from './Chai'
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1>Chai aur react</h1>
+      <Chai />
     </>
   )
 }
