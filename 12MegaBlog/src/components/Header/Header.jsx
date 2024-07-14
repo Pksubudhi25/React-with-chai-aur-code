3d1cb84b5315e3dc55bd46a360a00b8bb9652ce2
Pksubudhi25 <pratikkumarsubudhi@gmail.com>
2024-07-14 07:50:25 +0530

    Configuring redux toolkit and components

---
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Logo, LogoutBtn, Coontainer } from "../index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+function Header() {
+  // the status is in authslice
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  // These are the items to display in navigation bar when we want to add new item in navigation bar add new object of that item in navItems array
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
+  return (
+    <header className="py-3 shadow bg-gray-500">
+      <Coontainer>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Coontainer>
+    </header>
+  );
+}
+
+export default Header;
