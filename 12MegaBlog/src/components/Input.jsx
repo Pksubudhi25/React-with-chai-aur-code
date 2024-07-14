82fdf8996f1da0de1c0cc94193779617d024c527
Pksubudhi25 <pratikkumarsubudhi@gmail.com>
2024-07-14 07:51:27 +0530

    input element

--- 12MegaBlog/src/components/Input.jsx
+++ 12MegaBlog/src/components/Input.jsx
@@ -8,6 +8,7 @@ const Input = React.forwardRef(function Input({label,type="text",className='',..
                 label && <label
                 className='inline-block mb-1 pl-1'
                 htmlFor={id}
+                
                 >
                     {label}
                 </label>
